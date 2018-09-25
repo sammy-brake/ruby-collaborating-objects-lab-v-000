8cecf9cd6878e239db84994f510225a67a76c893
Samantha brake <sammy.brake@gmail.com>
2018-09-25 21:46:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,9 @@ class Song
   def self.new_by_filename(filename)
     split_files= filename.split(" - ")
     song = Song.new(split_files[1])
-    artist = split_files[0]
    
-    Artist.find_or_create_by_name(artist)
+   
+    Artist.find_or_create_by_name(split_files[0])
       song.artist = artist
     artist.add_song(song)
    
